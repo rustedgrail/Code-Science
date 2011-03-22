211332c0620ee3d97d93f3fcca74909c21c32a73
David Newell <beta@alumni.rice.edu>
2011-03-22 13:24:30 -0400

    DN: rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeScience::Application.config.secret_token = 'f63ece54527e2ad7b1152134c5b70cae16cd1212924d8dc6f5e8b030b185392700d5779877bea39b6ce902506dcacd72e39e89621f7bd73e1b9a63c959b1f884'
